02884ccb5db0e1deabcd3d3570385d125f917719
Alexander Toftesund <alexandertoftesund@gmail.com>
2025-09-28 03:11:05 +0200

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,25 @@ const giftBoxBottom = document.querySelector(".gift-box__bottom");
 const gift1 = document.querySelector(".gift-1");
 const gift2 = document.querySelector(".gift-2");
 const intro = document.querySelector(".intro");
+const arrow = document.querySelector(".arrow");
 
 const init = () => {
     setTimeout(() => {
-
             intro.classList.add("hide");
-
     }, 3000);
 }
 
 const animateBox = () => {
 
     giftBox.classList.add("gift-box__open");
+    arrow.style.filter = "opacity(0)";
 
     setTimeout(() => {
-
             gift1.classList.add("show");
-
     }, 1500);
 
     setTimeout(() => {
-
             gift2.classList.add("show");
-
     }, 3000);
 
 }
